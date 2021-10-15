49ebf090f5a47730d389f54a8f1162d85f910149
MrGretty <ruslantrufanovwork@gmail.com>
2021-10-15 18:33:18 +0300

    git create

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const usersRoute = require('./routes/users');
+
+require('dotenv').config();
+
+
+app.use(bodyParser.json());
+app.use(usersRoute);
+app.use((err, req, res, next) => {
+    if(err.statusCode) {
+        res.status(err.statusCode).send(err.message);
+    } else {
+        console.log(err);
+        res.status(500).send('Something unexpected happened');
+    }
+});
+// we will eventually use env variables
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log('Server Started');
+});
+
